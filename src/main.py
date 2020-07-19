4c7508d26316f002357ca790c581b1f8854a5e65
Francisco J. Moreno <franciscoj.sparcoltary@gmail.com>
2020-07-19 15:56:46 -0500

    Descifrado

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
+    # Abrimos el archivo
+    file = open('encoded.txt', 'r', encoding='utf-8')
+    # Validamos que el archivo se abre, con .read() vemos su contenido
+    # print(file.read())
+    # Creamos un patron de letras minusculas
+    patron = re.compile('[a-z]')
+    # Buscamos el patron en el archivo
+    matches = patron.findall(file.read())
+    # Imprimimos los matches
+    print(matches)
+    # Convertimos el arreglo en string
+    palabra = "".join(matches);
+    # Imprimimos el string
+    print(palabra)
+    
 
 
 if __name__ == '__main__':
